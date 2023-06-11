0f48e75623478a1b8f524e1cf32bc1ec3d658fbd
infectionScout <abimael.almonte8@gmail.com>
2023-06-11 17:58:06 -0400

    fixing directories

---
+++ include/globals.h
@@ -0,0 +1,14 @@
+#ifndef GLOBALS
+#define GLOBALS
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_scancode.h>
+#include <vector>
+#include <string>
+
+
+
+bool checkCollision(const SDL_Rect& rectA, const SDL_Rect& rectB);
+
+
+#endif
---
+++ include/player.h
@@ -0,0 +1,23 @@
+#ifndef PLAYER_H
+#define PLAYER_H
+#include <string>
+#include <SDL2/SDL.h>
+class Player{
+    public:
+        Player(int sw, int sh, int w, int h, std::string filePath, SDL_Renderer *renderer);
+        void LoadMedia(SDL_Renderer *renderer, std::string file);
+        void Flip(SDL_Renderer *renderer);
+        void mainLoop();
+        int x;
+        int y;
+        SDL_Rect src;
+        SDL_Rect dst;
+    private:
+        SDL_Texture* image;
+        
+
+
+};
+
+
+#endif
